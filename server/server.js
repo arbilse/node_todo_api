c8da008a617ecd520ff289973c885b8b72153971
arbi abeshi <arbilse@gmail.com>
2019-02-16 17:21:45 -0500

    add GET /todos route

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ var app = express();
 
 app.use(bodyParser.json());
 
+// TODOS - POST
 app.post('/todos', (req, res) => {
   var todo = new Todo({
     text: req.body.text
@@ -20,6 +21,18 @@ app.post('/todos', (req, res) => {
     res.status(400).send(e);
   });
 });
+
+// TODOS - GET
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (e) => {
+    res.status(400).send(e);
+  })
+});
+
+
+
 
 app.listen(3000, () => {
   console.log('Started on port 3000');
