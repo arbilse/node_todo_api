943d6a77ef4b14b47cebd9fe1baac8f2f8311cbd
arbi abeshi <arbilse@gmail.com>
2019-02-16 18:57:54 -0500

    add queries playground file

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,43 @@
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+// var id = '5c6892e08d50777c409d002d';
+//
+// if (!ObjectID.isValid(id)) {
+//   console.log('ID not Valid');
+// }
+
+// Todo.find({
+//   _id: id
+// }).then((todos) => {
+//   console.log('Todos', todos);
+// });
+//
+// Todo.findOne({
+//   _id: id
+// }).then((todo) => {
+//   console.log('Todo', todo);
+// });
+
+// Todo.findById(id).then((todo) => {
+//   if (!todo) {
+//     return console.log('Id not found');
+//   }
+//   console.log('Todo by id', todo);
+// }).catch((e) => console.log(e));
+
+
+// challenge - find USER
+var id = '5c675be48388e331643fde28';
+
+User.findById(id).then((user) => {
+  if (!user) {
+    return console.log('Unable to find user');
+  }
+  console.log(JSON.stringify(user, undefined, 2));
+}, (e) => {
+  console.log(e);
+});
