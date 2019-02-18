d26e1a6340f34bf3f3188192965e8161e811e182
arbi abeshi <arbilse@gmail.com>
2019-02-18 13:35:44 -0500

    add GET /users/me and authentication middleware

---
+++ server/middleware/authenticate.js
@@ -0,0 +1,22 @@
+var {User} = require('./../models/user');
+
+
+// middleware authentication function
+var authenticate = (req, res, next) => {
+  var token = req.header('x-auth');
+
+  User.findByToken(token).then((user) => {
+    if (!user) {
+      return Promise.reject();
+    }
+
+    req.user = user;
+    req.toke = token;
+    next();
+  }).catch((e) => {
+    res.status(401).send();
+  });
+};
+
+
+module.exports = {authenticate};
--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
-
+var {authenticate} = require('./middleware/authenticate');
 
 var app = express();
 const port = process.env.PORT;
@@ -120,6 +120,14 @@ app.post('/users', (req, res) => {
     res.status(400).send(e);
   })
 });
+
+
+// GET /users/me - private route
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user);
+});
+
+
 
 
 if(!module.parent) {     //implemented if(!module.parent) so test doesn't throw error
