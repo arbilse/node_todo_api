555d4c4ae98a583ddc33227e0dec900c457650bb
arbi abeshi <arbilse@gmail.com>
2019-02-17 19:48:18 -0500

    add patch /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
@@ -72,6 +73,36 @@ app.delete('/todos/:id', (req, res) => {
   });
 });
 
+
+// UPDATE /todos/id
+app.patch('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text', 'completed']);
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => { //deprecated
+  // Todo.findOneAndUpdate({_id:id}, {$set: body}, {new: true}).then((todo) => { //deprecated
+  // issue raised: https://github.com/Automattic/mongoose/issues/6880
+
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
 
 
 
