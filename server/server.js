04bb3760990f089a843e80f00fcb24f24269829a
arbi abeshi <arbilse@gmail.com>
2019-02-18 12:56:49 -0500

    add POST /users route and token method

--- server/server.js
+++ server/server.js
@@ -107,6 +107,20 @@ app.patch('/todos/:id', (req, res) => {
 });
 
 
+// POST /users
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body);
+
+  user.save().then(() => {
+      return user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth', token).send(user);
+  }).catch((e) => {
+    res.status(400).send(e);
+  })
+});
+
 
 if(!module.parent) {     //implemented if(!module.parent) so test doesn't throw error
   app.listen(port, () => {
